a4245eb687742150364f2c9abfcd79ab5b181541
bundasse <ymtotolove@gmail.com>
2025-06-14 22:45:20 +0900

    fix : 스크롤액션 수정

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let posArray = []
 let contents = document.getElementsByClassName("character")
 let talks= document.getElementsByClassName("talk")
+let iH = window.innerHeight
 
 window.scrollTo({top:0, behavior:'smooth'})
 
+var1 = window.innerWidth > 540 ? 100: iH*0.3;
+let posArray = []
+for(let i=1; i<talks.length; i++){
+    posArray.push(talks[i].getBoundingClientRect().y)
+}
 
 function addEvent() {
     window.addEventListener("scroll",() => {
-        let posArray = []
-        for(let i=1; i<talks.length; i++){
-            posArray.push(talks[i].getBoundingClientRect().top)
-        }
-        let iH = window.innerHeight
-        let idx = posArray.findIndex(e => {return iH >= e && e>300})
-
+        let scrY = window.scrollY
+        let idx = posArray.findIndex(e => {return iH+scrY >= e-100 && scrY <= e-iH+var1})
+        console.log(scrY+iH-var1, posArray)
         if(idx > -1){
             talks[idx+1].classList.add('slideIn')
             setTimeout(() => {
